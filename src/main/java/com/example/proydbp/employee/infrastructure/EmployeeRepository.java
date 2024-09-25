67fece48a036e0be83058b6616c6e962635e2dcf
fjmunozp <fernando.munoz.p@utec.edu.pe>
2024-09-25 18:53:45 -0500

    b

--- src/main/java/com/example/proydbp/employee/infrastructure/EmployeeRepository.java
+++ src/main/java/com/example/proydbp/employee/infrastructure/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.example.proydbp.employee.infrastructure;
 
 import com.example.proydbp.employee.domain.Employee;
+import com.example.proydbp.user.infrastructure.BaseUserRepository;
 
 public interface EmployeeRepository extends BaseUserRepository<Employee> {
 }
