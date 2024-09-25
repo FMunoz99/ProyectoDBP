9bf0e41c621f414c5d37830150137c17692f823e
fjmunozp <fernando.munoz.p@utec.edu.pe>
2024-09-25 18:20:33 -0500

    User

---
+++ src/main/java/com/example/proydbp/user/domain/User.java
@@ -0,0 +1,33 @@
+package com.example.proydbp.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "avg_rating", columnDefinition = "float default 0")
+    private Float avgRating;
+}
