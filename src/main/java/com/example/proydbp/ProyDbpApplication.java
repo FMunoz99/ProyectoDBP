36aeab8e2de58de60c12cb3beca16137fba25509
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-09-24 19:14:57 -0500

    hola

---
+++ src/main/java/com/example/proydbp/ProyDbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.proydbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyDbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyDbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proydbp/ProyDbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proydbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyDbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
