10ebdd52dbf0b8a7bd5622fb91027bb900ed6aff
fjmunozp <fernando.munoz.p@utec.edu.pe>
2024-09-25 18:44:23 -0500

    a

--- src/main/java/com/example/proydbp/client/domain/Client.java
+++ src/main/java/com/example/proydbp/client/domain/Client.java
@@ -1,5 +1,6 @@
 package com.example.proydbp.client.domain;
 
+import com.example.proydbp.user.domain.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
