dca92d10517743d7eee1a055db216360a094fe8e
fjmunozp <fernando.munoz.p@utec.edu.pe>
2024-09-25 18:44:23 -0500

    a

--- src/main/java/com/example/proydbp/client/infrastructure/ClientRepository.java
+++ src/main/java/com/example/proydbp/client/infrastructure/ClientRepository.java
@@ -1,6 +1,7 @@
 package com.example.proydbp.client.infrastructure;
 
 import com.example.proydbp.client.domain.Client;
+import com.example.proydbp.user.infrastructure.BaseUserRepository;
 
-public interface ClientRepository extends BaseUserRepository<Client>{
+public interface ClientRepository extends BaseUserRepository<Client> {
 }
